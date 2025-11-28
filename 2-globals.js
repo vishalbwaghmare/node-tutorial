dae82047b6a01ebd2b225f75da10955e800b6a80
SohelSayyad1572 <sohelmsayyad1572@gmail.com>
2025-11-28 09:02:32 +0530

    Initial commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW
+
+// __dirname    - path to curret directory
+// __fileName   - file name
+// require      - function to use module
+// module       - info about current module(file)
+// process      - info about env where program is being executed
+
+setInterval(() => {
+    console.log('Hello world')
+}, 1000)
---
+++ 3-module.js
@@ -0,0 +1,10 @@
+// Modules - Encapsulated Code(only share minimum)
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+//console.log(names)
+
+sayHi('vishal')
+sayHi(names.john)
+sayHi(names.peter)
