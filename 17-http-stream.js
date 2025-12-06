61df094f498c59bd2fb8a8a0d4bd57131d08c33d
SohelSayyad1572 <sohelmsayyad1572@gmail.com>
2025-12-06 13:50:26 +0530

    fixed duplicate code issue

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -9,29 +9,11 @@ http.createServer(function (req, res){
 
     const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
     fileStream.on('open',()=> {
-       fileStream.pipe() 
+       fileStream.pipe(res) 
     })
 
     fileStream.on('error', (err) => {
         res.end(err)
-    })var http = require('http')
-    
-    var fs = require('fs')
-    const { error } = require('console')
-    
-    http.createServer(function (req, res){
-        // const text = fs.readFileSync('./content/big.txt', 'utf8')
-        // res.end(text)
-    
-        const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
-        fileStream.on('open',()=> {
-           fileStream.pipe(res) 
-        })
-    
-        fileStream.on('error', (err) => {
-            res.end(err)
-        })
     })
-    .listen(3000)
 })
 .listen(3000)
